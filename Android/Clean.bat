d030195d71ab27a4d5ccbb19046ab16f877ade2e
Paketov <Shlixterberg@gmail.com>
2016-12-07 00:07:48 +0400

    updt

---
+++ Android/Clean.bat
@@ -0,0 +1,38 @@
+rd /S /Q %~dp0libs
+
+rd /S /Q %~dp0obj
+
+rd /S /Q %~dp0..\Modules\HttpConsoleShell\Android\libs
+
+rd /S /Q %~dp0..\Modules\HttpConsoleShell\Android\obj
+
+
+rd /S /Q %~dp0..\Modules\Example\Android\libs
+
+rd /S /Q %~dp0..\Modules\Example\Android\obj
+
+
+rd /S /Q %~dp0..\Modules\Logging\Android\libs
+
+rd /S /Q %~dp0..\Modules\Logging\Android\obj
+
+
+rd /S /Q %~dp0..\Modules\MdlAutoLoad\Android\libs
+
+rd /S /Q %~dp0..\Modules\MdlAutoLoad\Android\obj
+
+
+rd /S /Q %~dp0..\Modules\PortScanner\Android\libs
+
+rd /S /Q %~dp0..\Modules\PortScanner\Android\obj
+
+
+rd /S /Q %~dp0..\Modules\ReciverLog\Android\libs
+
+rd /S /Q %~dp0..\Modules\ReciverLog\Android\obj
+
+
+rd /S /Q %~dp0..\Modules\RemoteTerminal\Android\libs
+
+rd /S /Q %~dp0..\Modules\RemoteTerminal\Android\obj
+
